ac26585d02537927ef8ff0c0075913803ec55f6e
Amit Gupta <amit2606.gupta@gmail.com>
2024-07-23 22:28:34 +0530

    first commit

---
+++ variable.js
@@ -0,0 +1,45 @@
+fullName = "Tony Stark";
+age = 24;
+price = 150.50;
+console.log(fullName);
+
+
+
+
+
+
+//-------------------VAR------------------
+
+// var a = "Hello World";
+// var b = 10;
+// var c = 12;
+// var d = b + c;
+// console.log(a);  
+// console.log(b);
+// console.log(c); 
+// console.log(d);
+
+//------------Let-------------
+// let a = "Hello Amit";
+// let b = "Joining";
+// let c = " 12";
+// let d = b + c;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+
+//------------Const-----------
+
+const a= "Hello Amit";
+console.log(a);
+
+const b = 400;
+console.log(b);
+
+const c = "12";
+console.log(c);
+
+c = "new"
+console.log(c)
