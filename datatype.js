714e11c770756915fda01cafbba584c15414cbf5
Amit Gupta <amit2606.gupta@gmail.com>
2024-07-23 23:28:20 +0530

    implement data type

---
+++ datatype.js
@@ -0,0 +1,29 @@
+// const student = {
+//     fullName : "Amit Kumar",
+//     age : 24,
+//     cgpa: 7.5,
+//     isPass : true,
+
+
+// };
+// console.log(student.age);
+
+// const product = {
+//     title: "Parker Jotter Standerd CT Ball Pen",
+//     rating: 4,
+//     offer: 5,
+//     price: 270,
+    
+
+
+// };
+// console.log(product);
+
+const profile = {
+    userName: "@amit26.x",
+    isFollow: false,
+    followers: 200,
+    following: 300,
+
+};
+console.log(typeof profile);
