fdb3994455826928600535287bafe82301b244b8
Amit Gupta <amit2606.gupta@gmail.com>
2024-07-25 18:42:50 +0530

    Implement Operators and Conditional Statement

---
+++ conditional Statement.js
@@ -0,0 +1,84 @@
+// let age = 15;
+// if(age>=18){
+//     console.log("You can Vote");
+// }
+// if(age<18){
+//     console.log("You cannot Vote");
+// }
+
+
+// let mode = "light";
+// let color;
+// if(mode === "dark"){
+//     color = "black";
+// }
+// if(mode ==="light"){
+//     color = "white";
+// }
+// console.log(color);
+
+
+
+// let mode = "light";
+// let color;
+// if(mode === "dark"){
+//     color = "black";
+// } else{
+//     color = "white";
+// }
+// console.log(color);
+
+
+// let age = 25;
+// if(age >= 18){
+//     console.log("You Can Vote");
+// } else{
+//     console.log("You Cannot Vote");  
+// }
+
+
+// let num = 10;
+// if(num%2 === 0){
+//     console.log("Even")
+// } else{
+//     console.log("Odd");
+// }
+
+
+// let mode = "gold";
+// let color;
+// if(mode === "dark"){
+//     color = "black";
+// } else if(mode === "blue"){
+//     color = "blue";
+// } else if(mode === "pink"){
+//     color = "pink";
+// } else{
+//     color = "white";
+// }
+// console.log(color);
+
+
+//  let num = prompt("Enter a Number:");
+//  if(num % 5=== 0) {
+//     console.log(num, "is multiple of 5");
+//  }else {
+//     console.log(num, "is Not a multiple of 5");
+//  }
+
+
+
+// let score = prompt("Enter the Score (0-100):");
+// let grade;
+// if(score >= 90 && score <= 100){
+//     grade = "A";
+// } else if(score >= 70 && score <= 89 ){
+//     grade = "B";
+// } else if(score >= 60 && score <= 69){
+//     grade = "C";
+// } else if(score >= 50 && score <= 59){
+//     grade = "D";
+// }else if(score >= 0 && score <= 49 ){
+//     grade = "F";
+// }
+// console.log("According to your score grade was: ", grade);
