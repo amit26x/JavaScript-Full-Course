3274ab94147fd6109d70d4de1189ee224231d2a1
Amit Gupta <amit2606.gupta@gmail.com>
2024-07-27 22:27:03 +0530

    Implementing Variables

--- variable.js
+++ variable.js
@@ -1,45 +1,98 @@
-fullName = "Tony Stark";
-age = 24;
-price = 150.50;
-console.log(fullName);
+// var a = "Hello World";
+// var b = 10;
+// var c = 20;
+// var d = b + c;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+
+// let a = "Amit Gupta";
+// let b = " 10";
+// let c = " 12";
+// let d = b + c;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+
+// const a = "Hello World";
+// console.log(a);
+
+// const b = 400;
+// console.log(b);
+
+// const c = "12";
+// console.log(c);
+
+
+// let petName = "Rocky";
+// myFunction()
+
+// function myFunction(){
+//     fruit = 'apple';
+//     console.log(typeof petName + '-' + 'My pet name is ' + petName)
+// }
+// console.log(typeof petName + '-' + 'My pet name is ' + petName + 'Fruit name is ' + fruit)
+
+
+// myfunction();
+// anotherFunc();
+// let petName;
+// function myfunction(){
+//     let petName = "Sizzer";
+//     console.log(petName);
+// }
+// function anotherFunc(){
+//     let petName = "Tom";
+//     console.log(petName);
+
+// }
+// console.log(petName);
+
+
+// 
 
 
 
+//--------------Global Scope---------------
+// let num = 10;
+// console.log(num);
+// function fun(){
+//     console.log(num);
+// }
+// fun()
+
+
+//----------------Function Scope--------------
+
+// function fun(){
+//     let num = 10;
+//     console.log(num);
+// }
+// fun();
+// console.log(num);
 
 
 
-//-------------------VAR------------------
-
-var a = "Hello World";
-var b = 10;
-var c = 12;
-var d = b + c;
-console.log(a);  
-console.log(b);
-console.log(c); 
-console.log(d);
-
-//------------Let-------------
-let a = "Hello Amit";
-let b = "Joining";
-let c = " 12";
-let d = b + c;
-console.log(a);
-console.log(b);
-console.log(c);
-console.log(d);
+//---------------Redeclaring variables----------------
+// let x = 77;
+// {
+//     let x = 23;
+//     console.log(x);
+// }
+// console.log(x);
 
 
-//------------Const-----------
+//--------------Redeclaring Variables in the same blocks----------
 
-const a= "Hello Amit";
-console.log(a);
-
-const b = 400;
-console.log(b);
-
-const c = "12";
-console.log(c);
-
-c = "new"
-console.log(c)
+// let x = 77;
+// {
+//     let x = 23;
+//     console.log(x);
+// }
+// let x = 67;
+// console.log(x);
