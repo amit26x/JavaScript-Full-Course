6ca1ca66fcdc90691cdb11c430eb7bc055ee82ad
Amit Gupta <amit2606.gupta@gmail.com>
2024-08-02 11:02:51 +0530

    Learning JavaScript

--- function.js
+++ function.js
@@ -1,10 +1,20 @@
-function calculateDiscount(price, discountPercentage){
-    return price - (price * discountPercentage / 100);
+// function calculateDiscount(price, discountPercentage){
+//     return price - (price * discountPercentage / 100);
+// }
+
+
+// let originalPrice = 100;
+// let discount = 20;
+
+// let discountPrice = calculateDiscount(originalPrice, discount);
+// console.log("Original Price: " + originalPrice + ", Discounted Price: " + discountPrice);
+
+
+
+function verifyAge(age){
+    return age >= 18 ? "Adult" : "Minor"
+
 }
-
-
-let originalPrice = 100;
-let discount = 20;
-
-let discountPrice = calculateDiscount(originalPrice, discount);
-console.log("Original Price: " + originalPrice + ", Discounted Price: " + discountPrice);
+let userAge = 11;
+let verificationResult = verifyAge(userAge);
+console.log("The User is : " + verificationResult)
